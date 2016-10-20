2aab93a09ff8d45cda887c07df6e7e85a02f90f5
Evgen <e.salyakin@gmail.com>
2016-10-20 13:15:01 +0300

    push pop in my object

--- lesson_2/main.js
+++ lesson_2/main.js
@@ -1,1 +1,27 @@
+var arr =['a', 'b', 'c', 4, 5, 6];
+
+arr.push = function () {
+    for (var i=0, len = arguments.length, arrLenght = this.length; i<len; i++, arrLenght++) {
+        this[arrLenght] = arguments[i];
+    }
+    return this.length;
+};
+
+arr.pop = function () {
+    var arrLenght = this.length-1;
+    item = this[arrLenght];
+    delete this[arrLenght];
+    return item;
+}
+
+var len = arr.push('+',3);
+console.log('***  Push  ***');
+console.log(len);
+console.log(arr.toString());
+
+var item = arr.pop();
+console.log('***  Pop  ***');
+console.log(item);
+console.log(arr.toString());
+
 
