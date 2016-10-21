80dc9b5976daba7823b2605559dbf1a995233b15
Evgen <e.salyakin@gmail.com>
2016-10-21 18:18:54 +0300

    slice in my object

--- lesson_2/main.js
+++ lesson_2/main.js
@@ -11,17 +11,33 @@ arr.pop = function () {
     var arrLenght = this.length-1;
     item = this[arrLenght];
     delete this[arrLenght];
+    this.length = arrLenght;
     return item;
-}
+};
 
-var len = arr.push('+',3);
+arr.slice = function (begin, end) {
+    var result = [];
+    if (begin < 0) begin += this.length;
+    if (end < 0) end += this.length;
+    if ((begin<0) || (end<0) || (begin>end)) return result;
+    for (var i=0, n = begin; n<end; i++, n++) {
+        result[i] = this[n];
+    }
+    return result;
+};
+
 console.log('***  Push  ***');
+var len = arr.push('+',3);
 console.log(len);
 console.log(arr.toString());
 
-var item = arr.pop();
 console.log('***  Pop  ***');
+var item = arr.pop();
+console.log(item);
+item = arr.pop();
 console.log(item);
 console.log(arr.toString());
 
-
+console.log('***  Slice  ***');
+var newArr = arr.slice(2,-1);
+console.log(newArr.toString());
