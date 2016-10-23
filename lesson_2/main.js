74e8c805413fdcd9b52608ddf77ba0652ea6cba4
Evgen <e.salyakin@gmail.com>
2016-10-23 19:29:42 +0300

    finish reformat code

--- lesson_2/main.js
+++ lesson_2/main.js
@@ -1,62 +1,130 @@
 var arr = ['a', 'b', 'c', 4, 5, 6];
 var arrProto = ['a', 'b', 'c', 4, 5, 6];
 
-var myFunctions = {push: newPush, pop: newPop, slice: newSlice, join: newJoin, reverse: newReverse};
+var myFunctions = {push: newPush, pop: newPop, slice: newSlice, join: newJoin, reverse: newReverse, sum:mySum};
 Array.prototype.push = newPush;
 Array.prototype.pop = newPop;
 Array.prototype.slice = newSlice;
 Array.prototype.join = newJoin;
 Array.prototype.reverse = newReverse;
+Number.prototype.sum = mySum;
 
+// 1. push
 var len = myFunctions.push.apply(arr, ['+', 3]);
 var result1 = arr.toString();
 var lenProto = arrProto.push('+', 3);
 var resultProto1 = arrProto.toString();
 
+// 2. pop
+var item21 = myFunctions.pop.apply(arr);
+var item22 = myFunctions.pop.apply(arr);
+var result2 = arr.toString();
+var item23 = arrProto.pop();
+var item24 = arrProto.pop();
+var resultProto2 = arrProto.toString();
+
+// 3. slice
+var newArr = myFunctions.slice.apply(arr, [2, -1]);
+var newArrProto = arrProto.slice(-3);
+
+// 4. join
+var str = arr.join('+ ');
+var strProto = arrProto.join('* ');
+
+// 5. reverse
+var result5 = arr.reverse();
+var resultProto5 = arrProto.reverse();
+
+// 6. sum
+var x = 8;
+resSum1 = 5 .sum(6);
+resSum2 = x.sum('9','+');
+resSum3 = myFunctions.sum.apply(x,[6,2]);
+
+//******************** Console output ************************************
+// 1. push
 console.log('***  Push  ***');
-console.log("Function: len = myFunctions.push.apply(arr, ['+', 3])");
 console.log("Length: ", len);
 console.log("Result array: ", result1);
 console.log('-- prototype --');
-console.log("Function: len = arrProto.push('+', 3)");
 console.log("Length: ", lenProto);
 console.log("Result array: ", resultProto1);
 
-/*
+// 2. pop
 console.log('***  Pop  ***');
-var item = functionObject.pop.apply(arr);
-console.log("Item: ", item);
-item = functionObject.pop.apply(arr);
-console.log("Item: ", item);
-console.log("Result array: ", arrProto.toString());
+console.log("Item: ", item21);
+console.log("Item: ", item22);
+console.log("Result array: ", result2);
 console.log('-- prototype --');
-Array.prototype.pop = functionObject.pop;
-item = arrProto.pop();
-console.log("Item: ", item);
-item = arrProto.pop();
-console.log("Item: ", item);
-console.log("Result array: ", arrProto.toString());
+console.log("Item: ", item23);
+console.log("Item: ", item24);
+console.log("Result array: ", resultProto2);
 
+// 3. slice
 console.log('***  Slice  ***');
+console.log("New array: ", newArr);
 console.log('-- prototype --');
-Array.prototype.slice = functionObject.slice;
-var newArr = arrProto.slice(2, -1);
-console.log("New array: ", newArr.toString());
+console.log("New array: ", newArrProto);
 
+// 4. join
 console.log('***  Join  ***');
+console.log("Result string: ", str);
 console.log('-- prototype --');
-Array.prototype.join = functionObject.join;
-var str = arrProto.join('+ ');
-console.log("Result string: ", str.toString());
+console.log("Result string: ", strProto);
 
+// 5. reverse
 console.log('***  Reverse  ***');
+console.log("Result array: ", result5);
 console.log('-- prototype --');
-Array.prototype.reverse = functionObject.reverse;
-arrProto.reverse();
-console.log("Result array: ", arrProto.toString());
-*/
+console.log("Result array: ", resultProto5);
+
+// 6. sum
+console.log('***  Sum  ***');
+console.log('Result 1: ',resSum1);
+console.log('Result 2: ',resSum2);
+console.log('Result 3: ',resSum3);
+
+//******************** HTML output ************************************
+// 1. push
+document.getElementById('exp1').innerHTML += "len = myFunctions.push.apply(arr, ['+', 3])";
+document.getElementById('result1').innerHTML += "len = " + len + "; arr = " + result1;
+document.getElementById('exp1p').innerHTML += "len = arrProto.push('+', 3)";
+document.getElementById('result1p').innerHTML += "len = " + len + "; arrProto = " + resultProto1;
+
+// 2. pop
+document.getElementById('exp2').innerHTML += "item21 = myFunctions.pop.apply(arr); item22 = myFunctions.pop.apply(arr);";
+document.getElementById('result2').innerHTML += "item21 = " + item21 + "; item22 = " + item22 + "; arr = " + result2;
+document.getElementById('exp2p').innerHTML += "item23 = arrProto.pop(); item24 = arrProto.pop();";
+document.getElementById('result2p').innerHTML += "item23 = " + item23 + "; item24 = " + item24 + "; arrProto = " + resultProto2;
+
+// 3. slice
+document.getElementById('exp3').innerHTML += "newArr = myFunctions.slice.apply(arr, [2, -1]);";
+document.getElementById('result3').innerHTML += "newArr = " + newArr;
+document.getElementById('exp3p').innerHTML += "newArrProto = arrProto.slice(-3)";
+document.getElementById('result3p').innerHTML += "newArrProto = " + newArrProto;
+
+// 4. join
+document.getElementById('exp4').innerHTML += "str = arr.join('+ ');";
+document.getElementById('result4').innerHTML += "str = " + str;
+document.getElementById('exp4p').innerHTML += "strProto = arrProto.join('* ');";
+document.getElementById('result4p').innerHTML += "strProto = " + strProto;
+
+// 5. reverse
+document.getElementById('exp5').innerHTML += "result5 = arrProto.reverse();";
+document.getElementById('result5').innerHTML += "result5 = " + result5;
+document.getElementById('exp5p').innerHTML += "resultProto5 = arrProto.reverse();";
+document.getElementById('result5p').innerHTML += "resultProto5 = " + resultProto5;
+
+// 6. sum
+document.getElementById('exp6').innerHTML += "var x = 8; resSum1 = 5 .sum(6); resSum2 = x.sum('9','+');";
+document.getElementById('result6').innerHTML += "resSum1 = " + resSum1 + "; resSum2 = " + resSum2;
+document.getElementById('exp6p').innerHTML += "var x = 8; resSum3 = myFunctions.sum.apply(x,[6,2]);";
+document.getElementById('result6p').innerHTML += "resSum3 = " + resSum3;
+
+//******************** Functions ************************************
 function newPush() {
-    for (var i = 0, len = arguments.length, arrLenght = this.length; i < len; i++, arrLenght++) {
+    var len = arguments.length;
+    for (var i = 0, arrLenght = this.length; i < len; i++, arrLenght++) {
         this[arrLenght] = arguments[i];
     }
     return this.length;
@@ -70,7 +138,7 @@ function newPop() {
     return item;
 }
 
-function newSlice(begin, end) {
+function newSlice(begin=0, end=this.length) {
     var result = [];
     if (begin < 0) begin += this.length;
     if (end < 0) end += this.length;
@@ -100,4 +168,15 @@ function newReverse() {
         this[n] = item;
     }
     return this;
+}
+
+function mySum() {
+    var result = this;
+    if (arguments.length == 0) return result;
+    for (var i=0, len = arguments.length; i<len; i++) {
+        var item = arguments[i];
+        if (isNaN(item)) item = 0;
+        result += (+item);
+    }
+    return result;
 }
