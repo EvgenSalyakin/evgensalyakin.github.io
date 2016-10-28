85daaaf9031b1d06ec24cd473634be044f4f018d
Evgen <e.salyakin@gmail.com>
2016-10-29 00:52:06 +0300

    involition and sum

--- lesson_3/main.js
+++ lesson_3/main.js
@@ -1,10 +1,21 @@
 
-console.log('Factorial = ',factorial(5));
-console.log('Factorial = ',factorialSimple(10));
+//console.log('Factorial = ',factorial(20));
+//console.log('Factorial = ',factorialSimple(20));
 
-console.log(performanceMeasurement(factorial));
+console.log(performanceMeasurement(factorial,[20]));
+console.log(performanceMeasurement(factorialSimple,[20]));
 
-console.log(performanceMeasurement(factorialSimple));
+//console.log('5^6 = ',involution(5,6));
+//console.log('5^6 = ',involutionSimple(5,6));
+
+console.log(performanceMeasurement(involution,[5,6]));
+console.log(performanceMeasurement(involutionSimple,[5,6]));
+
+//console.log('3742: 3+7+4+2 = ',sumDigit(3742));
+//console.log('3742: 3+7+4+2 = ',sumDigitSimple(3742));
+
+console.log(performanceMeasurement(sumDigit,[374267833]));
+console.log(performanceMeasurement(sumDigitSimple,[374267833]));
 
 function factorial(n) {
     if (n==0) return 1;
@@ -17,9 +28,35 @@ function factorialSimple(n) {
     return rez;
 }
 
-function performanceMeasurement(func) {
+function involution(value,degree) {
+    if (degree==0) return 1;
+    if (degree==1) return value;
+    return value * involution(value,degree-1);
+}
+
+function involutionSimple(value,degree) {
+    var rez = 1;
+    for (var i = 1; i <= degree; ++i) rez *= value;
+    return rez;
+}
+
+function sumDigit(number) {
+    if (number==0) return 0;
+    var digit = number % 10;
+    return digit + sumDigit(number/10>>0);
+}
+
+function sumDigitSimple(number) {
+    var rez = 0;
+    var s = number.toString();
+    var lenght = s.length;
+    for (var i = 0; i < lenght; ++i) rez += (+s.charAt(i));
+    return rez;
+}
+
+function performanceMeasurement(func,arr) {
     var timeStampStart = Date.now();
-    for (var i=0;i<10000;i++) func(20);
+    for (var i=0;i<100000;i++) func.apply(null,arr);
     var timeStampFinish = Date.now();
     return timeStampFinish - timeStampStart;
 }
