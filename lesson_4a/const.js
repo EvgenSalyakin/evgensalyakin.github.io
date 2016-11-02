f6f68805f16e93eec9d51088f42a39a8a6f96d77
Evgen <e.salyakin@gmail.com>
2016-11-02 19:05:30 +0200

    Tamagotchi v.3.0 animate

--- lesson_4a/const.js
+++ lesson_4a/const.js
@@ -1,3 +1,4 @@
-const MAXPOWER = 8;
+const MAXPOWER = 10;
 const MAXAGE = 30;
+const STEPINTERVAL = 2500;
 var names = ["Hamster","Tom","BoomBox","Jack","Bit","Byte","Storm","Animal","Lucky","Poter","Script","Frankenstein","Demon","Gold"];
--- lesson_4a/main.js
+++ lesson_4a/main.js
@@ -1,19 +1,22 @@
-
 var i = getRandom(0,names.length-1);
 var tam = new Tamagotchi(names[i],MAXPOWER,isStep,isDie);
+
+// ['eat','drink','sleep','play','study','watchTV','work'];
+var src = [ 'http://smiles24.ru/data/smiles/smiles-eda-587.gif',
+            'http://smiles24.ru/data/smiles/smiles-pyushie-192.gif',
+            'http://smiles24.ru/data/smiles/smiles-son-194.gif',
+            'http://smiles24.ru/data/smiles/smiles-igry-25.gif',
+            'http://smiles24.ru/data/smiles/smiles-dumy-128.gif',
+            'http://smiles24.ru/data/smiles/smiles-televizor-15.gif',
+            'http://smiles24.ru/data/smiles/vremyzprovojdenie-183.gif'];
+
 var actions=tam.getMethods();
 
-// while (!tam.isDied()) {
-//     var r1 = getRandom(0,Math.floor(MAXPOWER/2));
-//     var r2 = getRandom(0,Math.floor(MAXPOWER/2));
-//     var ac = getRandom(0,actions.length-1);
-//     tam.go(r1).do(actions[ac],r2);
-// }
-
-var timerId = setInterval(tam.stepLife.bind(tam),500);
+var timerId = setInterval(tam.stepLife.bind(tam),STEPINTERVAL);
 
 function isDie() {
     clearInterval(timerId);
+    document.getElementById('tam').attributes.src.value = "http://smiles24.ru/data/smiles/smiles-cherti-141.gif";
 }
 
 function isStep() {
@@ -21,4 +24,5 @@ function isStep() {
     var r2 = getRandom(0,Math.floor(MAXPOWER/2));
     var ac = getRandom(0,actions.length-1);
     tam.go(r1).do(actions[ac],r2);
+    if (!tam.isDied()) document.getElementById('tam').attributes.src.value = src[ac];
 }
