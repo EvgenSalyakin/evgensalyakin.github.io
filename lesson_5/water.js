3c52252c7cb863e5b597e38dece936b5108020a4
Evgen <e.salyakin@gmail.com>
2016-11-13 01:56:04 +0200

    format

--- lesson_5/water.js
+++ lesson_5/water.js
@@ -1,32 +1,37 @@
-var height = [2,1,5,0,3,4,7,2,3,1,0];
+var height = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0];
 
-Array.prototype.max = function(){
-    return Math.max.apply( Math, this );
+Array.prototype.max = function () {
+    return Math.max.apply(Math, this);
 };
 
-Array.prototype.min = function(){
-    return Math.min.apply( Math, this );
+Array.prototype.min = function () {
+    return Math.min.apply(Math, this);
 };
 
+var water = waterValume(height);
+console.log('Water valume:' + water);
+document.getElementById('arr').innerHTML += height.toString();
+document.getElementById('result').innerHTML += water;
 
-var max = height.max();
-var min = height.min()+1;
-var water = 0;
+function waterValume(height) {
+    var max = height.max();
+    var min = height.min() + 1;
+    var water = 0;
 
-for (var i=min; i<=max; i++) {
-    var start=height.length-1;
-    var finish=0;
-    var kol=0;
-    for (var k=0;k<height.length;k++) {
-        if (height[k]>=i) {
-            kol++;
-            if (start>k) start = k;
-            if (finish<k) finish = k;
+    for (var i = min; i <= max; i++) {
+        var start = height.length - 1;
+        var finish = 0;
+        var kol = 0;
+        for (var k = 0; k < height.length; k++) {
+            if (height[k] >= i) {
+                kol++;
+                if (start > k) start = k;
+                if (finish < k) finish = k;
+            }
+        }
+        if (kol > 0) {
+            water += finish - start - kol + 1
         }
     }
-    if (kol>0) {water += finish-start-kol+1}
-    console.log('Start:'+start+' Finish:'+finish+' Kol:'+kol+' Water:'+water);
+    return water;
 }
-
-console.log('Max:'+max+' Min:'+min);
-console.log('Water:'+water);
