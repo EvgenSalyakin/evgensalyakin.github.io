12342139ad568e707ee49e0b6175862b1e421e54
Evgen <e.salyakin@gmail.com>
2016-11-28 00:25:39 +0200

    SVG water

--- lesson_5/water.js
+++ lesson_5/water.js
@@ -9,23 +9,21 @@ Array.prototype.min = function () {
 };
 
 var water = waterValume(height);
-console.log('Water valume (1):' + water);
+console.log('Water valume (1):' + water.water);
 document.getElementById('arr').innerHTML += height.toString();
-document.getElementById('result').innerHTML += water;
-
-var water = waterValume(height);
-console.log('Water valume (2):' + water);
+document.getElementById('result').innerHTML += water.water;
 
 function waterValume(height) {
-    var max = height.max();
-    var min = height.min() + 1;
-    var water = 0;
+    let max = height.max();
+    let min = height.min() + 1;
+    let water = 0;
+    let waterArr = [];
 
-    for (var i = min; i <= max; i++) {
-        var start = height.length - 1;
-        var finish = 0;
-        var kol = 0;
-        for (var k = 0; k < height.length; k++) {
+    for (let i = min; i <= max; i++) {
+        let start = height.length - 1;
+        let finish = 0;
+        let kol = 0;
+        for (let k = 0; k < height.length; k++) {
             if (height[k] >= i) {
                 kol++;
                 if (start > k) start = k;
@@ -33,13 +31,39 @@ function waterValume(height) {
             }
         }
         if (kol > 0) {
-            water += finish - start - kol + 1
+            water += finish - start - kol + 1;
+            for (let w=start;w<finish;w++) {
+                waterArr[w] = i;
+            }
         }
     }
-    return water;
+    return {"water":water, "waterArr":waterArr};
 }
 
-function waterValue2(height) {
-    var max = height.max();
+let svgEl = document.getElementById('mySvg');
+let maxheight = height.max()*30;
+svgEl.setAttribute("height",maxheight);
 
+for(let i=0;i<water.waterArr.length-1;i++){
+    let rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.setAttribute("x",i*30);
+    rect.setAttribute("y",maxheight-water.waterArr[i]*30);
+    rect.setAttribute("width","30");
+    rect.setAttribute("height",water.waterArr[i]*30);
+    rect.setAttribute("stroke","brown");
+    rect.setAttribute("fill","blue");
+
+    svgEl.appendChild(rect);
 }
+
+for(let i=0;i<height.length-1;i++){
+ let rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+ rect.setAttribute("x",i*30);
+ rect.setAttribute("y",maxheight-height[i]*30);
+ rect.setAttribute("width","30");
+ rect.setAttribute("height",height[i]*30);
+ rect.setAttribute("stroke","blue");
+ rect.setAttribute("fill","brown");
+
+ svgEl.appendChild(rect);
+ }
