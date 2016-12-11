3f5aa8334587175cb18bafabb0b6c1bd1410570f
Evgen <e.salyakin@gmail.com>
2016-12-11 11:40:36 +0200

    lesson 7

--- lesson_7/src/index.js
+++ lesson_7/src/index.js
@@ -9,16 +9,20 @@ import ToDoList from './todolistJQ';
 // window.jQuery = jQuery;
 import $ from 'jquery';
 import 'jquery-ui';
+import 'jquery-ui/ui/core';
+import 'jquery-ui/ui/widgets/draggable';
+import 'jquery-ui/ui/widgets/datepicker';
+import 'jquery-ui/ui/widgets/droppable';
 
-// $( "#datepicker" ).datepicker();
-// $( "#datepicker" ).datepicker("option", "dateFormat", "dd/mm/yy");
-// $(".task-container").droppable();
-// $(".todo-task").draggable({ revert: "valid", revertDuration:200 });
+
+
+$( "#datepicker" ).datepicker();
+$( "#datepicker" ).datepicker("option", "dateFormat", "dd/mm/yy");
+$(".task-container").droppable();
+//$(".todo-task").draggable();//{ revert: "valid", revertDuration:200 });
+
 
 let todolist = new ToDoList($);
 
 todolist.render();
-
-
-
-
+$('#btnadd').click(todolist.add.bind(todolist));
--- lesson_7/src/todolistJQ.js
+++ lesson_7/src/todolistJQ.js
@@ -1,3 +1,7 @@
+import 'jquery-ui/ui/core';
+import 'jquery-ui/ui/widget';
+import 'jquery-ui/ui/widgets/dialog';
+
 export default class {
     //ENTER_KEY : 13;
 
@@ -37,24 +41,68 @@ export default class {
         options = options || {};
         options = $.extend({}, this.defaults, options);
 
-        // $.each(data, function (index, params) {
-        //     generateElement(params);
-        // });
+        $.each(this.data, function (index, params) {
+             this.generateElement(params);
+        });
 
-        this.generateElement({
-         id: "123",
-         code: "1",
-         title: "asd",
-         date: "22/12/2013",
-         description: "Blah Blah"
-         });
+        // this.generateElement({
+        //  id: "123",
+        //  code: "1",
+        //  title: "asd",
+        //  date: "22/12/2013",
+        //  description: "Blah Blah"
+        //  });
 
+        $.each(this.codes, function (index, value) {
+            $(value).droppable({
+                drop: function (event, ui) {
+                    var element = ui.helper,
+                        css_id = element.attr("id"),
+                        id = css_id.replace(options.taskId, ""),
+                        object = this.data[id];
+
+                    // Removing old element
+                    this.removeElement(object);
+
+                    // Changing object code
+                    object.code = index;
+
+                    // Generating new element
+                    this.generateElement(object);
+
+                    // Updating Local Storage
+                    this.data[id] = object;
+                    localStorage.setItem("todoData", JSON.stringify(this.data));
+
+                    // Hiding Delete Area
+                    $("#" + this.defaults.deleteDiv).hide();
+                }
+            });
+        });
+
+        $("#" + options.deleteDiv).droppable({
+            drop: function(event, ui) {
+                var element = ui.helper,
+                    css_id = element.attr("id"),
+                    id = css_id.replace(options.taskId, ""),
+                    object = this.data[id];
+
+                // Removing old element
+                this.removeElement(object);
+
+                // Updating local storage
+                delete this.data[id];
+                localStorage.setItem("todoData", JSON.stringify(this.data));
+
+                // Hiding Delete Area
+                $("#" + this.defaults.deleteDiv).hide();
+            }
+        });
         // if (typeof window !== 'undefined') {
         //
         // }
     }
 
-    // Add Task
     generateElement(params){
         var parent = $(this.codes[params.code]),
             wrapper;
@@ -96,4 +144,83 @@ export default class {
         });
 
     };
+
+    removeElement(params) {
+        $("#" + this.defaults.taskId + params.id).remove();
+    };
+
+    add() {
+        var inputs = $("#" + this.defaults.formId + " :input"),
+            errorMessage = "Title can not be empty",
+            id, title, description, date, tempData;
+
+        if (inputs.length !== 4) {
+            return;
+        }
+
+        title = inputs[0].value;
+        description = inputs[1].value;
+        date = inputs[2].value;
+
+        if (!title) {
+            this.generateDialog(errorMessage);
+            return;
+        }
+
+        id = new Date().getTime();
+
+        tempData = {
+            id : id,
+            code: "1",
+            title: title,
+            date: date,
+            description: description
+        };
+
+        this.data[id] = tempData;
+        localStorage.setItem("todoData", JSON.stringify(this.data));
+
+        generateElement(tempData);
+
+         inputs[0].value = "";
+        inputs[1].value = "";
+        inputs[2].value = "";
+    };
+
+    generateDialog(message) {
+        var responseId = "response-dialog",
+            title = "Messaage",
+            responseDialog = $("#" + responseId),
+            buttonOptions;
+
+        if (!responseDialog.length) {
+            responseDialog = $("<div />", {
+                title: title,
+                id: responseId
+            }).appendTo($("body"));
+        }
+
+        responseDialog.html(message);
+
+        buttonOptions = {
+            "Ok" : function () {
+                responseDialog.dialog("close");
+            }
+        };
+
+        responseDialog.dialog({
+            autoOpen: true,
+            width: 400,
+            modal: true,
+            closeOnEscape: true,
+            buttons: buttonOptions
+        });
+    };
+
+    clear() {
+        this.data = {};
+        localStorage.setItem("todoData", JSON.stringify(this.data));
+        $("." + this.defaults.todoTask).remove();
+    };
+
 }
