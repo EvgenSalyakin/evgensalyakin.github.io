3d57f5c5a489ac30c652cacd2ca5a0142e1435ee
Evgen <e.salyakin@gmail.com>
2016-12-18 23:44:22 +0200

    lesson 9

--- lesson_9/model/map.js
+++ lesson_9/model/map.js
@@ -1,10 +1,12 @@
+var label;
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8,
         center: {lat: -34.397, lng: 150.644}
     });
     var geocoder = new google.maps.Geocoder();
-
+    label = 0;
     document.getElementById('submit').addEventListener('click', function() {
         geocodeAddress(geocoder, map);
     });
@@ -18,11 +20,21 @@ function geocodeAddress(geocoder, resultsMap) {
             var lat = results[0].geometry.location.lat();
             var lng = results[0].geometry.location.lng();
             resultsMap.setCenter(results[0].geometry.location);
+
+            var infowindow = new google.maps.InfoWindow({
+                content: '<p><b>'+address+'</b><br>lat/lng: '+lat+'/'+lng+'</p>'
+            });
+
+            label++;
             var marker = new google.maps.Marker({
                 map: resultsMap,
+                animation: google.maps.Animation.DROP,
                 position: results[0].geometry.location,
                 title: address+' lat/lng : '+lat+'/'+lng,
-                label: lat+'/'+lng
+                label: label.toString()
+            });
+            marker.addListener('click', function() {
+                infowindow.open(resultsMap, marker);
             });
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
---
+++ lesson_9/model/qr-code.js
@@ -0,0 +1,27 @@
+function getQR() {
+    var url = encodeURI($('#qr-input')[0].value);
+    $.ajax({
+        url: "https://pierre2106j-qrcode.p.mashape.com/api",
+        type: "GET",
+        data: {
+            //backcolor=ffffff&ecl=H&forecolor=000000&pixel=4&text=https%3A%2F%2Fevgensalyakin.github.io%2Flesson_9%2Findex.html&type=url
+            backcolor:"ffffff",
+            ecl:"H",
+            forecolor:"000000",
+            pixel:"4",
+            text:url,
+            type:"url"
+        },
+        beforeSend: function(xhr){
+            xhr.setRequestHeader('X-Mashape-Key', 'm5zusdFTO0mshFerGv4AqGfh8majp19NHsrjsnawcYKE4q2qsw');
+            xhr.setRequestHeader('Accept', 'text/plain');
+        },
+        success: function(result) {
+            document.getElementById('qr-img').setAttribute('src',result);
+        }
+    });
+};
+
+$('#qr-submit').click(getQR);
+
+getQR();
