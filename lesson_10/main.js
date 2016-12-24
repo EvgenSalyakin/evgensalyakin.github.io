9b77814f0db81395610f653808cc364d92a42583
Evgen <e.salyakin@gmail.com>
2016-12-24 20:23:58 +0200

    lesson 10 - materializecss

--- lesson_10/main.js
+++ lesson_10/main.js
@@ -2,8 +2,15 @@ $('#newsBTN').click(function () {
     getNews();
 });
 
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+    getNews().then(function () {
+        $('.carousel').carousel();
+    });
+});
+
 function getNews() {
-    $.ajax({
+    return $.ajax({
         method: 'GET',
         cache: false,
         url: 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.pravda.com.ua%2Frss%2F',
@@ -21,38 +28,23 @@ function getNews() {
 function processingNews(data) {
     var news = [];
     var text = "";
-    var arrLong = [];
-    var arrShort = [];
-    var arrMost = [];
     var arrWord = [];
     var exceptions = ["а","і","в","у","на","під","з","та","про","вже","від","без","поки","коли","який","якої","його",
-        "ще","якщо","їх","через"];
+        "ще","якщо","їх","через","до","не","що","й","за","ле","то","майже","понад","як","так","ні"];
 
-    //document.getElementById("result").innerHTML += items.title;
+    $(".carousel").empty();
     data.items.forEach(function (item) {
         news.push(item.title);
-        document.getElementById("result").innerHTML += "<p>" + item.title + "</p>";
         text += " " + item.title;
+        $(".carousel").append("<a class='carousel-item' ><p>" + item.title + "</p></a>");
     });
     text = text.trim();
     var reg = /[а-яА-ЯіІєЄїЇ'’]+/ig;
 
     text.match(reg).forEach(function (word) {
         word = word.toLowerCase();
-        // if (long == undefined) long = word;
-        // if (shot == undefined) shot = word;
-        //
-        // if (long.length < word.length) long = word;
-        // if (shot.length > word.length) shot = word;
 
         if (exceptions.indexOf(word)!=-1) return;
-        //var index = arrWord.indexOf(word);
-
-        // for (var i = 0; i < exceptions.length; i++) {
-        //     if (exceptions[i].word == word) {
-        //         return;
-        //     }
-        // }
 
         var index = -1;
         for (var i = 0; i < arrWord.length; i++) {
@@ -72,10 +64,6 @@ function processingNews(data) {
         return 0;
     });
 
-    // arr.forEach(function (item) {
-    //     console.log(item.word);
-    // });
-
     var long = '';
     var short = '';
     for (var i = 0, n = arr.length-1; i<4; i++, n--) {
@@ -85,10 +73,8 @@ function processingNews(data) {
     short = short + arr[i].word;
     long = long + arr[n].word;
 
-    console.log("Short " + short);
-
-    document.getElementById("short-word").innerHTML = "<p>Short: " + short + "</p>";
-    document.getElementById("long-word").innerHTML = "<p>Long: " + long + "</p>";
+    document.getElementById("short-word").innerHTML = "<p>" + short + "</p>";
+    document.getElementById("long-word").innerHTML = "<p>" + long + "</p>";
 
 
     arr = arrWord.sort(function (a, b) {
@@ -97,14 +83,10 @@ function processingNews(data) {
         return 0;
     });
 
-    arr.forEach(function (item) {
-         console.log(item.word+" : "+item.count);
-    });
-
     var most = '';
     for (var i = 0; i<4; i++) {
         most = most + arr[i].word +' : '+arr[i].count+', ';
     };
     most = most + arr[i].word+' : '+arr[i].count;
-    document.getElementById("most-word").innerHTML = "<p>Most: " + most + "</p>";
+    document.getElementById("most-word").innerHTML = "<p>" + most + "</p>";
 }
