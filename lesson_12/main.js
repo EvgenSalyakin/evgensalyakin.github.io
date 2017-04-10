b57dc9bd23fee43c48006bfe69e02726dbbf6b45
EvgenSalyakin <Fsji578>
2017-04-10 11:25:25 +0300

    lesson 13 lesson 13

--- lesson_12/main.js
+++
@@ -1,6 +0,0 @@
-let PortfolioApp = angular.module('PortfolioApp', []);
-
-PortfolioApp.controller('PortfolioCtrl', function PortfolioCtrl($scope) {
-    console.log('Portfolio controller !');
-    $scope.totalScills = 4;
-});
